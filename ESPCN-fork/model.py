e84309e2859f4283b68136a0c9a11c45f77a2b50
john <john.rickard@student.uni-siegen.de>
2020-08-02 21:40:19 +0200

    Add Module class for NN with XY coordinates

--- ESPCN-fork/model.py
+++ ESPCN-fork/model.py
@@ -18,6 +18,22 @@ class Net(nn.Module):
         return x
 
 
+class NetXY(nn.Module):
+    def __init__(self, upscale_factor):
+        super(NetXY, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 64, (5, 5), (1, 1), (2, 2))
+        self.conv2 = nn.Conv2d(64, 32, (3, 3), (1, 1), (1, 1))
+        self.conv3 = nn.Conv2d(32, 3 * (upscale_factor ** 2), (3, 3), (1, 1), (1, 1))
+        self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
+
+    def forward(self, x):
+        x = F.tanh(self.conv1(x))
+        x = F.tanh(self.conv2(x))
+        x = F.sigmoid(self.pixel_shuffle(self.conv3(x)))
+        return x
+
+
 if __name__ == "__main__":
     model = Net(upscale_factor=3)
     print(model)
